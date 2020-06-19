d26c7e2f06d9e56d2952646a13fa30b628eb7285
ironlich1 <mario.aguila_70@hotmail.com>
2020-06-19 19:07:18 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Perfil',
+      url: '/folder/Perfil',
+      icon: 'person'
+    },
+    {
+      title: 'Dispositivos',
+      url: '/folder/Dispositivos',
+      icon: 'wifi'
+    },
+    {
+      title: 'Historial',
+      url: '/folder/Historial',
+      icon: 'reader'
+    },
+    {
+      title: 'Tienda on-line',
+      url: '/folder/Tienda Online',
+      icon: 'cart'
+    },
+    {
+      title: 'Ayuda',
+      url: '/folder/Ayuda',
+      icon: 'help'
+    }
+  ];
+  //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private menu: MenuController
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  async run(){
+    var URL = "C:\Users\IronLich\Documents\ClotGuard\src\assets\Logo1.jpg"
+  }
+}
